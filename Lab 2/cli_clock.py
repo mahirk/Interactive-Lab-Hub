1095e992f51c61c01af05d003fb3ffeb6ba3b514
Mahir Kothary <m.kothary95@gmail.com>
2021-09-16 13:27:46 -0400

    add utf8 icons

--- Lab 2/cli_clock.py
+++ Lab 2/cli_clock.py
@@ -2,4 +2,6 @@ from time import strftime, sleep
 while True:
     print (strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True)
     print("\r", end="", flush=True)
+    cmd = "curl -s wttr.in/?format=2"
+    print(subprocess.check_output(cmd, shell=True).decode("utf-8"))
     sleep(1)
