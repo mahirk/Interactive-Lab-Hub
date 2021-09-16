27ca4b97d0d197cb0f51caac5107a4c47f4a076e
Mahir Kothary <m.kothary95@gmail.com>
2021-09-16 15:04:14 -0400

    add more fonts

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -8,26 +8,39 @@ import geocoder
 import requests
 import datetime
 
-g = geocoder.ip('me')
-latitude = str(g.latlng[0])
-longitude = str(g.latlng[1])
+def get_time_from_epoch(val):
+    timestamp = datetime.datetime.fromtimestamp(val)
+    return str(timestamp.strftime("%H:%M"))
 
-url = "https://api.openweathermap.org/data/2.5/onecall"
+def load_weather_data():
+    g = geocoder.ip('me')
+    latitude = str(g.latlng[0])
+    longitude = str(g.latlng[1])
 
-querystring = {
-    "lat":latitude,
-    "lon":longitude,
-    "exclude":"hourly,minutely",
-    "appid":"33a1154b742cf3f8f1a8e4ecd4341183"
-}
+    weatherUrl = "https://api.openweathermap.org/data/2.5/onecall"
 
-headers = {
-    'cache-control': "no-cache",
-}
-response = requests.request("GET", url, headers=headers, params=querystring)
+    querystring = {
+        "lat":latitude,
+        "lon":longitude,
+        "exclude":"hourly,minutely",
+        "appid":"33a1154b742cf3f8f1a8e4ecd4341183"
+    }
 
-print(response.json())
-weatherData = response.json()
+    headers = {
+        'cache-control': "no-cache",
+    }
+
+    response = requests.request("GET", weatherUrl, headers=headers, params=querystring)
+    return response.json()
+
+def convert_kelvin_to_farenheight(kelvin):
+    d_f = (kelvin − 273.15) × 9/5 + 32
+    return "{} °F".format(d_f)
+
+
+imagesUrl = "https://openweathermap.org/img/wn/{}.png"
+
+weatherData = load_weather_data()
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -77,32 +90,38 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font = ImageFont.truetype("roboto/Roboto-Regular.ttf", 18)
 timeSize = 30
-fontTime = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", timeSize)
+fontTime = ImageFont.truetype("roboto/Roboto-Regular.ttf", timeSize)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
-epochtime = weatherData['current']['sunrise']
-datetime = str(datetime.datetime.fromtimestamp(epochtime))
-print(datetime)
+sunrise_in_epoch = weatherData['current']['sunrise']
+sunrise_str = "Sunrise: " + get_time_from_epoch(sunrise_in_epoch)
+
+sunset_in_epoch = weatherData['current']['sunset']
+sunset_str = "Sunset: " + get_time_from_epoch(sunset_in_epoch)
+
+print(sunrise_str)
+print(sunset_str)
+
+dateValue = "Date: " + time.strftime("%m/%d/%Y")
 
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     y = top
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
-    dateValue = time.strftime("%m/%d/%Y")
     timeValue = time.strftime("%H:%M:%S")
 
     draw.text((x, y), dateValue, font=font, fill="#FFFFFF")
     y += font.getsize(dateValue)[1] + 14
     draw.text((x, y), timeValue, font=fontTime, fill="#FFFFFF")
     y += font.getsize(timeValue)[1] + 14
-    draw.text((x, y), datetime, font=font, fill="#FFFFFF")
+    draw.text((x, y), sunrise_str, font=font, fill="#FFFFFF")
 
     # Display image.
     disp.image(image, rotation)
